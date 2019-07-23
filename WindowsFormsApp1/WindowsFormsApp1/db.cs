35d819af2e3f426e53b1214b0825c1b0c8ed00c9
HirushaR <hirusharandunu11@gmail.com>
2019-07-23 09:58:06 +0530

    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/db.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    class db
+    {
+        public static SqlConnection GetConnection()
+        {
+            // this is database class
+            string conn =@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\hiru\works\C#\HMS\hotel.mdf;Integrated Security=True;Connect Timeout=30";
+            SqlConnection mycon = new SqlConnection(conn);
+            return mycon;
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/reception.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class reception:login
+    {
+        public override int log(string unm, string psw)
+        {
+            return 0;
+        }
+    }
+}
