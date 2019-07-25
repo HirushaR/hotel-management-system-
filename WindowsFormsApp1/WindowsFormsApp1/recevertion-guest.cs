52ab2de60ae781ba742135dd5125b3be87364307
HirushaR <hirusharandunu11@gmail.com>
2019-07-25 09:06:17 +0530

    modify

--- WindowsFormsApp1/WindowsFormsApp1/recevertion-guest.cs
+++ WindowsFormsApp1/WindowsFormsApp1/recevertion-guest.cs
@@ -29,26 +29,31 @@ namespace WindowsFormsApp1
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            string fnm = textBox2.Text;
+            string fnm = textBox2.Text; 
             string lnm = textBox3.Text;
             string desig = textBox4.Text;
             string add1 = textBox5.Text;
             string add2 = textBox6.Text;
             string country = textBox7.Text;
-            string date = textBox8.Text;
+            string bdate = textBox8.Text;
+            string nic = textBox14.Text;
+            string passport = textBox15.Text;
+            string email = textBox17.Text;
+            string nationality = textBox18.Text;
             string remarks = textBox9.Text;
             string likes = textBox10.Text;
             string diss = textBox11.Text;
             string gutitle = textBox12.Text;
-            string status = textBox13.Text;
-            string nic = textBox14.Text;
-            string passport = textBox15.Text;
+            string status = textBox13.Text; 
             string tp = textBox16.Text;
-            string email = textBox17.Text;
-            string nationality = textBox18.Text;
+                      
             string creator = Form1.unm;
-            string cdate = DateTime.Now.ToString("dddd, dd MMMM yyyy");
-            MessageBox.Show("" + cdate + ""+ creator);
+            string cdate = DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss");
+
+            using (var connection = db.GetConnection())
+            {
+                string qry = "insert into guests values('"+fnm+"','"+lnm+"',)";
+            }
         }
     }
 }
